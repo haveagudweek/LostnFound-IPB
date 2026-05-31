8d7a9e0c0a541b496f900f6f4fd70704a17fb37e
Yasir <justabigy@gmail.com>
2026-05-31 13:53:01 +0700

    fix(auth): cant connect to db and be

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -1,5 +1,5 @@
 from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import HTTPBearer
 from jose import JWTError, jwt
 from sqlalchemy.orm import Session
 from app.cores.database import get_db
@@ -7,17 +7,19 @@ from app.cores.config import settings
 from app.models.user import User
 from app.schemas.user import TokenData
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login", auto_error=False)
+security = HTTPBearer(auto_error=False)
 
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+def get_current_user(credentials = Depends(security), db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
     
-    if token is None:
+    if credentials is None:
         raise credentials_exception
+        
+    token = credentials.credentials
     
     try:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
